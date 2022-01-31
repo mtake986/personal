759807569430e72bdb626e7f3c77c5b5d0ef98f1
Masahiro Takechi <tak21011@byui.edu>
2022-01-31 16:10:19 -0700

    get a number users want to convert, show results

--- sprint2_java/Main.java
+++ sprint2_java/Main.java
@@ -2,7 +2,7 @@ import java.util.Scanner;  // Import the Scanner class
 import java.util.ArrayList;
 
 class Main {
-  public static Float show_measurements(ArrayList<String> list) {
+  public static String show_and_select_measurements(ArrayList<String> list) {
     // for(String element : list) {
     //   System.out.println(element);
     // }
@@ -10,21 +10,41 @@ class Main {
     for(int i=0; i<list.size(); i++) {
       System.out.println((i+1) +". " + list.get(i));
     }
-    Scanner obj = new Scanner(System.in);
-    String measurements_which = obj.nextLine();
-    System.out.println("Your pick => " + list.get(Integer.parseInt(measurements_which)-1));
-    list.remove(Integer.parseInt(measurements_which)-1);
-    while (true) {
-      System.out.println("Type a number from 999.9 to -999.9: ");
-      Scanner val_obj = new Scanner(System.in);
-      float val_obj_to_float = val_obj.nextFloat();
-      if (-999.9 <= val_obj_to_float && val_obj_to_float <= 999.9) {
-        return val_obj_to_float;
+    while(true) {
+      Scanner obj = new Scanner(System.in);
+      int measurements_num = obj.nextInt();
+      if (1 <= measurements_num && measurements_num <= list.size()) {
+        System.out.println("Your pick => " + list.get(measurements_num-1));
+        String selected_measurement = list.get(measurements_num-1);
+        list.remove(measurements_num-1);
+        return selected_measurement;
       } else {
         error("----- Error: Invalid number -----");
       }
     }
-
+  }
+  public static float get_number(String selected) {
+    while (true) {
+      if (selected.equals("temp")) {
+        System.out.println("Type a number from -999.9 to 999.9: ");
+        Scanner val_obj = new Scanner(System.in);
+        float val_obj_to_float = val_obj.nextFloat();
+        if (-999.9 <= val_obj_to_float && val_obj_to_float <= 999.9) {
+          return val_obj_to_float;
+        } else {
+          error("----- Error: Invalid number -----");
+        }
+      } else {
+        System.out.println("Type a number from 0 to 99,999.9: ");
+        Scanner val_obj = new Scanner(System.in);
+        float val_obj_to_float = val_obj.nextFloat();
+        if (0 <= val_obj_to_float && val_obj_to_float <= 999.9) {
+          return val_obj_to_float;
+        } else {
+          error("----- Error: Invalid number -----");
+        }
+      }
+    }
   }
   static void error(String msg) {
     System.out.println(msg);
@@ -32,9 +52,17 @@ class Main {
   static void endMsg() {
     System.out.println("Thanks for using. Have a wonderful day!!");
   }
+  static void show_result(ArrayList<String> list, float value, ArrayList<Float> multiplier, String selected){
+    for (int i = 0; i < list.size(); i++) {
+      float result = value*multiplier.get(i);
+      System.out.println("----- Result: " + value + " " + selected + " => " + String.format("%.2f", result) + " " + list.get(i) + " -----");
+    }
+  }
 
   public static void main(String[] args) {
-    System.out.println("Welcome to a coverter app. ");
+    System.out.println("--------------------------------------");
+    System.out.println("----- Welcome to a coverter app. -----");
+    System.out.println("--------------------------------------");
     
     while (true) {
       Scanner obj = new Scanner(System.in);  // Create a Scanner object
@@ -52,14 +80,54 @@ class Main {
             ArrayList<String> weight = new ArrayList<>();
             weight.add("kg");
             weight.add("lbs");
-            float value = show_measurements(weight);
-            System.out.println(value);
+            weight.add("oz");
+            // get a selected measurement, value
+            String selected = show_and_select_measurements(weight);
+            float value = get_number("weight");
+            // System.out.println("This is a list: " + weight);
+            // System.out.println("This is a seleceted measurement: " + selected);
+            // System.out.println("This is a value: " + value);
+            switch (selected){
+              case "kg":
+                ArrayList<Float> w_multiplier_kg = new ArrayList<Float>() {{
+                  add(2.2f);
+                  add(35.27f);
+                }};
+                show_result(weight, value, w_multiplier_kg, selected);
+                break;
+              case "lbs":
+                ArrayList<Float> w_multiplier_lbs = new ArrayList<Float>() {{
+                  add(.45f);
+                  add(16f);
+                }};
+                show_result(weight, value, w_multiplier_lbs, selected);
+                break;
+              case "oz":
+                ArrayList<Float> w_multiplier_oz = new ArrayList<Float>() {{
+                  add(0.0283f);
+                  add(0.0625f);
+                }};
+                show_result(weight, value, w_multiplier_oz, selected);
+                break;
+            }
             break;
           } else if (choice.equals("2")){
             ArrayList<String> temp = new ArrayList<>();
             temp.add("°C");
             temp.add("°F");
-            show_measurements(temp);
+            // get a selected measurement, value
+            String selected = show_and_select_measurements(temp);
+            float value = get_number("temp");
+            switch (selected){
+              case "°C":
+                float temp_converted_value_to_f = value / 5 * 9 + 32;
+                System.out.println("----- Result: " + value + selected + " => " + String.format("%.1f", temp_converted_value_to_f) + temp.get(0) + " -----");
+                break;
+              case "°F":
+                float temp_converted_value_to_c = (value - 32 ) / 9 * 5;
+                System.out.println("----- Result: " + value + selected + " => " + String.format("%.1f", temp_converted_value_to_c) + temp.get(0) + " -----");
+                break;
+            }
             break;
           } else if (choice.equals("3")) {
             ArrayList<String> length = new ArrayList<>() {{
@@ -67,18 +135,44 @@ class Main {
               add("feet");
               add("cm");
             }};
-            show_measurements(length);
+            // get a selected measurement, value
+            String selected = show_and_select_measurements(length);
+            float value = get_number("length");
+            switch (selected){
+              case "inch":
+                ArrayList<Float> len_multiplier_in = new ArrayList<Float>() {{
+                  add(.083f);
+                  add(2.54f);
+                }};
+                show_result(length, value, len_multiplier_in, selected);
+                break;
+              case "feet":
+                ArrayList<Float> len_multiplier_ft = new ArrayList<Float>() {{
+                  add(12f);
+                  add(30.48f);
+                }};
+                show_result(length, value, len_multiplier_ft, selected);
+                break;
+              case "cm":
+                ArrayList<Float> len_multiplier_cm = new ArrayList<Float>() {{
+                  add(0.39f);
+                  add(0.033f);
+                }};
+                show_result(length, value, len_multiplier_cm, selected);
+                break;
+            }
             break;
           } else {
-            error("----- Error: Invalid input. Type one of [weight, temp, length]. -----");
+            error("----- Error: Invalid number -----");
           }
         }
       } else if (quit_continue.equals("2")) {
         endMsg();
         break;
       } else {
-        error("----- Error: Invalid input. Type either [continue, quit]. -----");
+        error("----- Error: Invalid number -----");
       }
+      System.out.println("===============================================");
     }
   }
 }
--- sprint2_java/todo.md
+++ sprint2_java/todo.md
@@ -9,3 +9,21 @@
 1. ask users to quit or continue 
 - if quit, break the while loop and tell bye bye 
 - if continue, show measurements list.
+
+``` java
+for (int i = 0; i < weight.size(); i++) {
+  System.out.println("----- Result: " + value + " " + selected + " => " + value*w_multiplier_kg.get(i) + " " + weight.get(i) + " -----");
+}
+for (int i = 0; i < weight.size(); i++) {
+  float result = value*w_multiplier_kg.get(i);
+  System.out.println("----- Result: " + value + " " + selected + " => " + String.format("%.2f", result) + " " + weight.get(i) + " -----");
+}
+
+show_result(weight, value, w_multiplier_kg, selected);
+static void show_result(ArrayList<String> list, float value, ArrayList<Float> multiplier, String selected){
+  for (int i = 0; i < list.size(); i++) {
+    float result = value*multiplier.get(i);
+    System.out.println("----- Result: " + value + " " + selected + " => " + String.format("%.2f", result) + " " + list.get(i) + " -----");
+  }
+}
+```
